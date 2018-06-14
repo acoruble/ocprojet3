9b0d26ce91e64c297abfc4737af0c716c0c16a81
Anne-Lise Coruble <38162196+acoruble@users.noreply.github.com>
2018-06-14 16:18:51 +0200

    Add files via upload

--- carte.js
+++ carte.js
@@ -8,18 +8,22 @@ function initMap() {
     center: new google.maps.LatLng(45.763773, 4.8454991),
     mapTypeId: 'terrain'
   });
-  // Create an array of alphabetical characters used to label the markers.
   $.ajax({
     url: 'https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=978ca551b43dc6408bf48568dbc1f47392eba830',
     type: 'GET',
     dataType: 'text',
     success: function retour(results) {
       console.log("ça marche !");
+      var listeMarqueurs =[];
       JSON.parse(results).forEach(function(station) {
         var marker = new google.maps.Marker({
           position: station.position,
           map: map,
         });
+        listeMarqueurs.push(marker);
+      });
+      var markerCluster = new MarkerClusterer(map, listeMarqueurs, {
+        imagePath: 'marqueurs/m'
       });
     },
   });
